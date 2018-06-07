ada75495ce97602a9658ac9acee15e0815967aa5
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-06-07 09:43:03 -0700

    add task list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { FormsModule }  from '@angular/forms';
+import { TaskListComponent } from './task-list/task-list.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
